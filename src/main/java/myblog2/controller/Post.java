4ff58e3c21800b7b4b1b9b6e2540807da23e6a09
biswajita12345 <biswajita200300@gmail.com>
2024-01-25 00:50:26 +0530

    127:create post save feature entity class

---
+++ src/main/java/myblog2/controller/Post.java
@@ -0,0 +1,22 @@
+package myblog2.controller;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
+@Entity
+@Data
+public class Post {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
